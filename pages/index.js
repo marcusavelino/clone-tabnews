fb447a323e3dea5c3f6535ecab4294c3a12363f5
MARCUS VINICIUS DINIZ AVELINO <marcusavelino@gmail.com>
2024-11-23 13:02:21 +0000

    Adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+    return <h1>Precisamos nos cercar de pessoas que nos falem o que precisamos ouvir e não o que queremos ouvir, mas ao mesmo tempo não nos deem a impressão do impossível!</h1>
+}
+
+export default Home;
